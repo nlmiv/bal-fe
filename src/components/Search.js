bc0c6b772e96028963efa9b9237b7823da8b1b62
nlmiv <napmanaog@aim.com>
2021-08-10 17:07:06 +1000

    initial project setup

---
+++ src/components/Search.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+class Search extends Component {
+
+  render() {
+    return (
+      <div>
+        Search Flight: Coming Soon
+      </div>
+    );
+  }
+
+}
+
+export default Search;
